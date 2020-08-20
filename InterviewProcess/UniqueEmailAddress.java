ade3341807747e27032a99f8620e8223dd195931
Patricia Juane <patriciajuane@gmail.com>
2020-08-20 09:40:58 +0000

    Initial commit

---
+++ InterviewProcess/UniqueEmailAddress.java
@@ -0,0 +1,24 @@
+/*Online Assessment Question*/
+
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        if(emails == null || emails.length == 0) {
+            return 0;
+        }
+	
+        Set<String> seen = new HashSet();
+        for (String email : emails) {
+          int i = email.indexOf('@');
+          String local = email.substring(0, i);
+          String domain = email.substring(i);
+          if (local.contains("+")) {
+            local = local.substring(0, local.indexOf('+'));
+          }
+          // Note: one should escape the specific character '.', since it is treated as a regex expression.
+          local = local.replaceAll("\\.", "");
+          seen.add(local + domain); // reformated email
+        }
+
+        return seen.size();
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main { 
+
+ 
+
+	public static void main(String[] args) { 
+    Main m = new Main();
+    m.insert(5);
+    
+	} 
+} 
