f5a2f5b66154bc171d4aa4a0fd6e848335a2f542
Patricia Juane <patriciajuane@gmail.com>
2020-08-31 08:49:27 +0000

    Arrays and Exercises

---
+++ ArraysAndStrings/KthLargestElement.java
@@ -0,0 +1,19 @@
+class Solution {
+//1st alternative -> use a sorting algorithm and return k element
+//2nd alternative: increase time complexity using additional space
+
+    public int findKthLargest(int[] nums, int k) {
+        // init heap 'the smallest element first'
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+
+        // keep k largest elements in the heap
+        for (int n: nums) {
+          heap.add(n);
+          if (heap.size() > k)
+            heap.poll();
+        }
+
+        // output
+        return heap.poll();        
+  }
+}
