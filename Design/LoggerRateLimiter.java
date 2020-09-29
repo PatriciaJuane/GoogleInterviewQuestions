0927cf38596dc5c695a444307455968919622db5
Patricia Juane <patriciajuane@gmail.com>
2020-09-29 08:49:36 +0000

    Design exercises

---
+++ Design/LoggerRateLimiter.java
@@ -0,0 +1,33 @@
+class Logger {
+    
+    private HashMap<String, Integer> map;
+
+    /** Initialize your data structure here. */
+    public Logger() {
+        map = new HashMap<>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(!this.map.containsKey(message)) {
+            this.map.put(message, timestamp);
+            return true;
+        }
+        
+        Integer valor = this.map.get(message); // devuelve el valor
+        if(timestamp - valor >= 10) {
+            this.map.put(message,timestamp);
+            return true;
+        } else 
+            return false;
+        
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
